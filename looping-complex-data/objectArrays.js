564cf4c7ebfe342e4bd94083ed86b1f8c2e66ca9
Robert Phillips <robert_phillips17@outlook.com>
2022-07-01 15:17:48 +0100

    nested objects

--- looping-complex-data/objectArrays.js
+++ looping-complex-data/objectArrays.js
@@ -1,7 +1,8 @@
 const fruitObjectArray = [
-    { fruit: 'apple' },
-    { fruit: 'banana' },
-    { fruit: 'orange' }
+    { fruit: 'apple', rating: 8, price: 150 },
+    { fruit: 'banana', rating: 5, price: 100 },
+    { fruit: 'orange', rating: 6, price: 120 },
+    { fruit: 'pineapple', rating: 10, price: 300 }
 ]
 
 console.log(fruitObjectArray[1].fruit)
@@ -12,3 +13,33 @@ const fruitValueArray = fruitObjectArray.map(fruitObject => {
 })
 
 console.log(fruitValueArray)
+
+const searchTerm = 'apple'
+
+const onlyApples = fruitObjectArray.filter(fruitObject => {
+    const fruitMatch = fruitObject.fruit.includes(searchTerm)
+    return fruitMatch
+})
+
+console.log(onlyApples)
+
+const fruitList = ['apple', 'pear', 'banana', 'cherry', 'kiwi']
+
+const generateHtml = (array) => {
+    const arrayHtml = array.map(item => `<li>${item}</li>`)
+    return arrayHtml.join('\n')
+}
+
+console.log(generateHtml(fruitList))
+
+console.log(fruitObjectArray[2].price)
+
+const fruitFormatter = (fruitObject) => {
+    const formattedFruitObject = fruitObject.map(object =>
+        object.price = `£${(object.price / 100).toFixed(2)}`
+        )
+
+        return formattedFruitObject
+}
+
+console.log(fruitFormatter(fruitObjectArray))
