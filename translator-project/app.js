2e62ee7fbde66d8562015c687043700381c12346
Robert Phillips <robert_phillips17@outlook.com>
2022-07-11 17:33:16 +0100

    first commit

---
+++ translator-project/app.js
@@ -0,0 +1,18 @@
+class Translator {
+    constructor(input, language) {
+        this.input = input
+        this.language = language
+    }
+
+    checkInput() {
+
+    }
+
+    translateToEnglish() {
+
+    }
+
+    translateToMorse() {
+
+    }
+}
