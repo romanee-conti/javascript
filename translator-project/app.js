08944e53d5190487c5410eb1e136f9682676ca49
Robert Phillips <robert_phillips17@outlook.com>
2022-07-12 17:14:59 +0100

    Basic html, css and outline of class complete

--- translator-project/app.js
+++ translator-project/app.js
@@ -1,18 +1,61 @@
 class Translator {
-    constructor(input, language) {
-        this.input = input
-        this.language = language
+    constructor(inputTextElement, outputTextElement) {
+        this.inputTextElement = inputTextElement
+        this.outputTextElement = outputTextElement
+        this.clear()
+
+        this.alphabet = [
+            ["A", ".-"], ["B", "-..."], ["C", "-.-."], ["D", "-.."], ["E", "."], ["F", "..-."], ["G", "--."], ["H", "...."], ["I", ".."], ["J", ".---"],
+            ["K", "-.-"], ["L", ".-.."], ["M", "--"], ["N", "-."], ["O", "---"], ["P", ".--."], ["Q", "--.-"], ["R", ".-."], ["S", "..."], ["T", "-"], ["U", "..-"],
+            ["V", "...-"], ["W", ".--"], ["X", "-..-"], ["Y", "-.--"], ["Z", "--.."], ["1", ".----"], ["2", "..---"], ["3", "...--"], ["4", "....-"], ["5", "....."],
+            ["6", "-...."], ["7", "--..."], ["8", "---.."], ["9", "----."], ["0", "-----"], [".", ".-.-.-"], [",", "--..--"], ["?", "..--.."], ["'", ".----."], ["!", "-.-.--"],
+            ["/", "-..-."], [":", "---..."], [";", "-.-.-."], ["=", "-...-"], ["-", "-....-"], ["_", "..--.-"], ["\"", ".-..-."], ["@", ".--.-."], ["(", "-.--.-"], [" ", " "]
+        ];
     }
 
-    checkInput() {
+    setLanguage(inputLanguage) {
+        this.inputLanguage = inputLanguage
+        if (this.inputLanguage === "English") {
+            this.inputTextElement.placeholder = "Type your text here..."
+            this.isEnglishToMorse = true
+        }
 
+        if (this.inputLanguage === "Morse") {
+            this.inputTextElement.placeholder = "Type Morse code using the '.' and '-' characters. Seperate words with spaces (' ') and letters with a slash ('/')."
+            this.isEnglishToMorse = false
+        }
     }
 
-    translateToEnglish() {
-
+    clear() {
+        this.input = ""
+        this.output = ""
     }
 
-    translateToMorse() {
+    // translateToEnglish() {
+    //     const englishOutput = '';
+    // }
 
+    // translateToMorse() {
+    //     const englishArray = this.input.split("")
+    //     const morseOutput = englishArray.map(char => {
+    //     })
+
+    // }
+
+    updateDisplay() {
+        this.inputTextElement.value = this.input
+        this.outputTextElement.innerText = this.output
     }
 }
+
+const inputLanguage = document.querySelector('.input-language')
+const inputTextElement = document.querySelector('.text-input')
+const outputTextElement = document.querySelector('.text-output')
+
+const translator = new Translator(inputTextElement, outputTextElement)
+
+inputLanguage.addEventListener('change', () => {
+    translator.setLanguage(inputLanguage.value);
+    translator.clear();
+    translator.updateDisplay();
+})
