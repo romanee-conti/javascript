c23f723b76d6bb7e6ebd78bde70a70b41b920efc
Robert Phillips <robert_phillips17@outlook.com>
2022-07-21 17:07:40 +0100

    began work on menus

---
+++ games/space-war/app.js
@@ -0,0 +1,14 @@
+const mothershipImg = document.querySelector('[data-mothership-img]')
+const mothershipDiv = document.querySelector('[data-mothership-div')
+
+mothershipImg.addEventListener('mouseover', () => {
+    mothershipDiv.innerHTML = `
+        <ul class="menu">
+            <li>Mothership</li>
+            <li>HP: 100 / 100</li>
+        </ul >`
+})
+
+mothershipImg.addEventListener('mouseout', () => {
+    mothershipDiv.style.backgroundColor = ''
+})
