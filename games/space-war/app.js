dbf7856bd65aeb84199fb2d9eeefc4354f16622b
Robert Phillips <robert_phillips17@outlook.com>
2022-07-22 10:08:25 +0100

    Some styling changes

--- games/space-war/app.js
+++ games/space-war/app.js
@@ -2,8 +2,8 @@ const mothershipImg = document.querySelector('[data-mothership-img]')
 const mothershipDiv = document.querySelector('[data-mothership-div')
 
 mothershipImg.addEventListener('mouseover', () => {
-    mothershipDiv.innerHTML = `
-        <ul class="menu">
+    mothershipDiv.innerHTML += `
+        <ul class="stats-menu">
             <li>Mothership</li>
             <li>HP: 100 / 100</li>
         </ul >`
