52228ac69c27aa4ee5f8d97aeaf3780c2cd41498
Robert Phillips <robert_phillips17@outlook.com>
2022-10-05 12:18:57 +0100

    favicon added

--- react/ticket-tracker/src/App.jsx
+++ react/ticket-tracker/src/App.jsx
@@ -6,8 +6,20 @@ import NavBar from './components/NavBar';
 const App = () => {
 
   return (
-    <div>App working...</div>
+    <>
+    <nav className={styles.nav}>
+    < NavBar />
+    </nav>
+    <section className={styles.content}>
+      < EmployeeCard />
+    </section>
+    </>
   );
 };
 
 export default App;
+
+// pseudo-code plan
+// need to create a card element
+// mechanism to iterate over employee list using props
+// useState for the counter component
---
+++ react/ticket-tracker/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './NavBar.module.scss';
+
+const NavBar = () => {
+  return (
+    <nav>
+        <h1>Ticket Tracker</h1>
+    </nav>
+  )
+}
+
+export default NavBar
