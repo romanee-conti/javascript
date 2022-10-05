59f88fc6eb8addb1a37f0e1c5a33be53d2bfcd39
Robert Phillips <robert_phillips17@outlook.com>
2022-10-05 14:29:17 +0100

    styling and unicode for minus symbol added

--- react/ticket-tracker/src/components/Counter/Counter.jsx
+++ react/ticket-tracker/src/components/Counter/Counter.jsx
@@ -5,7 +5,13 @@ const Counter = () => {
     const [count, setCount] = useState(0);
     return (
         <div className={styles.counter}>
-            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount(count - 1)}><>&#8722;</></button>
+
+            {
+            /* in order to render HTML entities, such as Unicode and characters
+            not on a standard keyboard, you must wrap the code inside fragments <>&#0000</> */    
+            }
+
             <h1>{count}</h1>
             <button onClick={() => setCount(count + 1)}>+</button>
         </div>
