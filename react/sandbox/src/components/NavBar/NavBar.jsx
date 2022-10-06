7af1d64030e8b70cc7ea263bf3c289c2a064d0a5
Robert Phillips <robert_phillips17@outlook.com>
2022-10-06 14:06:32 +0100

    page not rerendering despite url updating

--- react/sandbox/src/components/NavBar/NavBar.jsx
+++ react/sandbox/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './NavBar.module.scss';
+import { Link } from '@reach/router';
 
 const NavBar = () => {
   return (
     <nav>
+      <Link to="/">
         <a href="">Home</a>
+      </Link>
+
+      <Link to="login">
         <a href="">Login</a>
+      </Link>
+
+      <Link to="favourites">
         <a href="">Favourites</a>
+      </Link>
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar
